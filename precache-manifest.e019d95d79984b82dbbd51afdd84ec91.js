ff04bb22ecdb659e128ede8cc0bcb670a3f22a7e
Diego <diegorl84@gmail.com>
2020-04-15 22:16:48 -0500

    Updates

---
+++ precache-manifest.e019d95d79984b82dbbd51afdd84ec91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8542a5e0d43fe0ec9eab0d3856718da",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "9d15e0d2b07a7d928644",
+    "url": "/react1/static/css/main.f31312f6.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/react1/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react1/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d15e0d2b07a7d928644",
+    "url": "/react1/static/js/main.316a7e55.chunk.js"
+  },
+  {
+    "revision": "fbb7010b95f0f28cb317",
+    "url": "/react1/static/js/runtime-main.14999f78.js"
+  }
+]);
